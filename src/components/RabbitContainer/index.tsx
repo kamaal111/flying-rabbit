0124e3cc7b39846020c176e9533c35ac83ccc10e
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-30 17:37:48 +0200

    Added proptypes

--- src/components/RabbitContainer/index.tsx
+++ src/components/RabbitContainer/index.tsx
@@ -1,5 +1,5 @@
-import { default as React, useState, useEffect } from 'react';
-import { View, Image, Animated, Easing, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { default as React, useState } from 'react';
+import { Animated, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 import types from './types';
 import { screenHeight, screenWidth } from '../../dimensions';
@@ -41,9 +41,9 @@ const Rabbit = ({ rabbitSource }): JSX.Element => {
           borderBottomWidth: 2,
         }}
       />
-      <TouchableOpacity onPressIn={() => console.log('press in')}>
-        <Animated.Image style={{ ...styles.rabbitImage, top: count }} source={rabbitSource} />
-      </TouchableOpacity>
+
+      <Animated.Image style={{ ...styles.rabbitImage, top: count }} source={rabbitSource} />
+
       <TouchableOpacity
         onPressIn={() => setCount(count + 30)}
         style={{
